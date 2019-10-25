bb47cc982904ee0f2d20bd759b4ac387b7b82c7d
Vitaliy Sheyanov <vit.onix@gmail.com>
2019-10-26 00:37:01 +0300

    Initial commit

---
+++ LocationDemo/src/main/java/net/arwix/location/demo/LocationMainActivity.kt
@@ -0,0 +1,12 @@
+package net.arwix.location.demo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LocationMainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_location_main)
+    }
+}
