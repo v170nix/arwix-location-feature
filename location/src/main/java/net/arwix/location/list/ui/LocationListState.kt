1e520ea0fb16f905845a0e3430e641956d4c45d9
Vitaliy Sheyanov <vit.onix@gmail.com>
2020-11-02 04:56:30 +0300

    update location

--- location/src/main/java/net/arwix/location/list/ui/LocationListState.kt
+++ location/src/main/java/net/arwix/location/list/ui/LocationListState.kt
@@ -4,7 +4,6 @@ import net.arwix.location.data.room.LocationTimeZoneData
 
 data class LocationListState(
     val isAutoLocationUpdate: Boolean = false,
-//    val autoLocationTimeZoneData: LocationTimeZoneData? = null,
     val autoLocationPermission: LocationPermission = LocationPermission.DeniedRationale,
     val isSelected: Boolean = false,
     val locationList: List<LocationTimeZoneData>? = null
